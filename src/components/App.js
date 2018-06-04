772ac5d280e4cf380e8f2ff287ad8a783b600d6b
Hardik Desai <hardik.desai@causecode.com>
2018-06-04 14:32:51 +0530

    animations

--- src/components/App.js
+++ src/components/App.js
@@ -4,37 +4,51 @@ import './App.css';
 import AgeStat from './agestat';
 
 
+
 class App extends Component {
 
   constructor(){
     super();
     this.state={ 
       userdate:'',
-      bdate:'1996-06-02'
+      bdate:'1996-06-02',
+      showStat:false
     }
   }
   changeval()
   {
     console.log(this.state);
     this.setState({bdate:this.state.userdate});
+    this.setState({showStat:true})
     console.log(this.state.bdate);
   }
 
   render() {
     return (
       <div className="App">
-         
-           <h1>Age Teller</h1>
+         <center>
+         <h1>Age Teller</h1>
+         <br/>
            
-              <Form inline >
+            <Form className="fc" >
+              
                 <FormControl type="date" onChange={event => this.setState({userdate: event.target.value})}></FormControl>
+                <br/>
                 <Button bsStyle="danger" onClick={()=> this.changeval()}>
-                  submit
+                  Tell my age
                 </Button>
-                <AgeStat date={this.state.bdate}/>
-              </Form>
-          
-       </div>
+                
+            </Form><br/>{
+            this.state.showStat?
+            <div className="fade age-stats">
+            <AgeStat date={this.state.bdate}/>
+            </div>:
+            <div></div>
+            
+            }
+
+            </center>
+        </div>
     );
   }
 }
--- src/components/agestat.js
+++ src/components/agestat.js
@@ -1,12 +1,28 @@
 import React,{Component} from 'react';
-
+import congo from './cg.jpg';
+import './age.css';
 
 class AgeStat extends Component{
+    TimeOnEarth(date)
+    {
+     let today =new Date().getTime();
+     let other_date=new Date(date).getTime();
+     let difference=Math.abs(today-other_date);
+     let days=Math.floor(difference/(1000*3600*24));
+     let years=Math.floor(days/365);
+     days -= years*365;
+     let moth=Math.floor(days/31);
+     days -= moth*31;
+     return `${years} years, ${moth} months, and ${days} days`
+    }
     
 render(){
 return(
-<div  className="AgeStat">
-   <h3>{this.props.date}</h3>
+<div className="AgeStat">
+<center>
+   <h3>{this.TimeOnEarth(this.props.date)}</h3>
+   
+   <img src={congo} alt="congo" className="congo" /></center>
 </div>
 );
 }
